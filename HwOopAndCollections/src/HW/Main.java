a7fa7d55471f8cf29e14d4db29a65160b096580c
S-MW <72529306+S-MW@users.noreply.github.com>
2021-11-17 02:50:32 +0300

    Solved

--- HwOopAndCollections/src/HW/Main.java
+++ HwOopAndCollections/src/HW/Main.java
@@ -1,8 +1,14 @@
 package HW;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("--- Circle ---");
         Circle obj1 = new Circle();
         System.out.println(obj1.getRadius());
@@ -12,6 +18,65 @@ public class Main {
         Cylinder obj2 = new Cylinder();
         System.out.println(obj2.getHeight());
         System.out.println(obj2.getVolume());
+
+        System.out.println("====================================================");
+        System.out.println("====================================================");
+        System.out.println("====================================================");
+
+        System.out.println("--- Before replace second element ---");
+        ArrayList <Integer> arrList1 = new ArrayList<>();
+        arrList1.add(10);
+        arrList1.add(20);
+        arrList1.add(30);
+        arrList1.add(40);
+        arrList1.add(50);
+        System.out.println(arrList1);
+        System.out.println("--- After replace second element ---");
+        arrList1.remove(1);
+        arrList1.add(1,98);
+        System.out.println(arrList1);
+
+        System.out.println("-------------------------");
+        PriorityQueue <String> pq1 = new PriorityQueue<>();
+        pq1.offer("Blue");
+        pq1.offer("Red");
+        pq1.offer("Black");
+
+        Iterator <String> iterator1 = pq1.iterator();
+        while (iterator1.hasNext())
+        {
+            System.out.println(iterator1.next());
+        }
+        System.out.println("-------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue <String> pq2 = new PriorityQueue<>();
+
+        System.out.println("Enter any thing name or [Exit] to finish the loop");
+        while (true)
+        {
+            System.out.print("Input :");
+            String userInput = sc.next();
+
+            if(userInput.equals("Exit"))
+            {
+                System.out.println("Exit the loop !");
+                System.out.println("All names you Add : " + pq2);
+                return;
+            }
+            else
+            {
+                    if(pq2.contains(userInput))
+                    {
+                        System.out.println("Sorry, This name is add before !");
+                    }
+                    else
+                    {
+                        pq2.add(userInput);
+                    }
+            }
+
+        }
 
 
     }
